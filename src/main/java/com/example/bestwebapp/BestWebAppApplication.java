5b3e5171d9700f17c74b119a8cbd8b934664f62e
Janek1010 <janekdomozych99@gmail.com>
2023-06-06 17:51:42 +0200

    creation

---
+++ src/main/java/com/example/bestwebapp/BestWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bestwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bestwebapp/BestWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bestwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
