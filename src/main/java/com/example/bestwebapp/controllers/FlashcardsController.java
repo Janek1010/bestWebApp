18bb0cc36d0bffe2c7f556c4eebbc3b446748a04
Janek1010 <janekdomozych99@gmail.com>
2023-06-06 22:16:40 +0200

    removing frontend dir

--- src/main/java/com/example/bestwebapp/controllers/FlashcardsController.java
+++ src/main/java/com/example/bestwebapp/controllers/FlashcardsController.java
@@ -3,12 +3,16 @@ package com.example.bestwebapp.controllers;
 import com.example.bestwebapp.model.Flashcard;
 import com.example.bestwebapp.services.FlashcardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -37,6 +41,5 @@ public class FlashcardsController {
         return flashcardService.findFlashcardById(id).orElseThrow(ChangeSetPersister.NotFoundException::new);
         //dodac potem implementacje NotFoundException wlasna
     }
-
 
 }
