ba0e4a00aacc236b746fe0f173bdb9bba6843cf8
Janek1010 <janekdomozych99@gmail.com>
2023-06-06 21:59:46 +0200

    adding flashcards

--- src/main/java/com/example/bestwebapp/bootstrap/BootstrapData.java
+++ src/main/java/com/example/bestwebapp/bootstrap/BootstrapData.java
@@ -13,7 +13,17 @@ public class BootstrapData implements CommandLineRunner {
     private final FlashcardRepository flashcardRepository;
     @Override
     public void run(String... args) throws Exception {
-        flashcardRepository.save(Flashcard.builder().name("name1").category(Type.POLISH).description("ciułałą").build());
-        flashcardRepository.save(Flashcard.builder().name("name2").category(Type.POLISH).description("bejbebejbe").build());
+
+        flashcardRepository.save(Flashcard.builder().name("Wielki Odpoczynek").category(Type.POLISH).description("Okres relaksu po ciężkiej pracy.").build());
+        flashcardRepository.save(Flashcard.builder().name("Smakowita Kolacja").category(Type.POLISH).description("Wykwintne danie podane wieczorem.").build());
+        flashcardRepository.save(Flashcard.builder().name("Wzorowy Uczeń").category(Type.POLISH).description("Osoba, która osiąga wysokie wyniki w nauce.").build());
+        flashcardRepository.save(Flashcard.builder().name("Złota Jesień").category(Type.POLISH).description("Piękna pora roku, charakteryzująca się złocistymi liśćmi na drzewach.").build());
+        flashcardRepository.save(Flashcard.builder().name("Magiczna Chwila").category(Type.POLISH).description("Niepowtarzalny moment pełen czarującej atmosfery.").build());
+        flashcardRepository.save(Flashcard.builder().name("Wspaniała Przygoda").category(Type.POLISH).description("Niezwykłe doświadczenie pełne ekscytacji i emocji.").build());
+        flashcardRepository.save(Flashcard.builder().name("Mistrzowski Ruch").category(Type.POLISH).description("Doskonałe wykonanie pewnego ruchu lub manewru.").build());
+        flashcardRepository.save(Flashcard.builder().name("Słoneczne Wakacje").category(Type.POLISH).description("Okres urlopu spędzany w ciepłym i słonecznym miejscu.").build());
+        flashcardRepository.save(Flashcard.builder().name("Złoty Środek").category(Type.POLISH).description("Optymalne rozwiązanie lub kompromis między dwoma skrajnościami.").build());
+        flashcardRepository.save(Flashcard.builder().name("Doskonały Koncert").category(Type.POLISH).description("Wyjątkowe wykonanie muzyczne, które zachwyca słuchaczy.").build());
+
     }
 }
