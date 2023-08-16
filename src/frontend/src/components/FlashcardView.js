442d4e287b6b018874b8e619b61454d6d107cd2c
jotzet <s24317@pjwstk.edu.pl>
2023-08-16 12:03:50 +0200

    styling for the flashcards component

--- src/frontend/src/components/FlashcardView.js
+++ src/frontend/src/components/FlashcardView.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { flashcardsMocks } from '../mocks/flashcardsMocks';
+import { Box, Button, Card, Typography, CardContent, Stack } from '@mui/joy';
 
-function FlashcardView({ data }) {
+function FlashcardView() {
 
-    data = flashcardsMocks; //change to API when it's ready
+    const data = flashcardsMocks; //change to API when it's ready
+    
     const [showData, setShowData] = useState(false);
 
     const handleShowData = () => {
@@ -15,30 +17,48 @@ function FlashcardView({ data }) {
     };
 
     return (
-        <div>
-            {!showData && (
-                <button id="show-data-btn" onClick={handleShowData}>
-                    Show
-                </button>
-            )}
-
-            {showData && (
-                <div>
-                    <button id="hide-data-btn" onClick={handleHideData}>
-                        Hide
-                    </button>
-                    <div id="show-flashcards">
-                        {data.map((flashcard, index) => (
-                            <div key={index} className="flashcard-element">
-                                <p>name: {flashcard.name}</p>
-                                <p>description: {flashcard.description}</p>
-                                <p>category: {flashcard.category}</p>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            )}
-        </div>
+            <Box>
+                {!showData ? (
+                    <Button
+                        id="show-data-btn"
+                        variant="contained"
+                        onClick={handleShowData}
+                    >
+                        Show
+                    </Button>
+                ) : (
+                    <Box>
+                        <Button
+                            id="hide-data-btn"
+                            variant="contained"
+                            onClick={handleHideData}
+                        >
+                            Hide
+                        </Button>
+                        <Stack id="show-flashcards"
+                         direction="row"
+                         spacing={2}
+                         justifyContent="center"
+                        >
+                            {data.map((flashcard, index) => (
+                                <Card color="neutral" variant="outlined" key={index} className="flashcard-element">
+                                       <CardContent orientation="vertical">
+                                    <Typography variant="h6">
+                                        Name: {flashcard.name}
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        Description: {flashcard.description}
+                                    </Typography>
+                                    <Typography variant="body2">
+                                        Category: {flashcard.category}
+                                    </Typography>
+                                    </CardContent>
+                                </Card>
+                            ))}
+                        </Stack>
+                    </Box>
+                )}
+        </Box>
     );
 }
 
